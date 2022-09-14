17865ab4e17125a8f867520c907b61665ee752c5
Mikhail <41973795+cappelchi@users.noreply.github.com>
2022-09-14 15:17:23 -0700

    added: .gitignore

---
+++ calcio.py
@@ -0,0 +1,16 @@
+
+'''
+Команды скрипта
+ 1. Развернуть окружение
+ 2. Обновить модель
+ 3. Получить предикт
+ 4. Обновить результаты
+
+Установки
+gensim=4.2.0
+neptune-client
+'''
+
+import neptune.new as neptune
+import pandas as pd
+import numpy as np
