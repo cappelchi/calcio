735373a234d693a296098d4aba8103c689f4cff3
Mikhail <41973795+cappelchi@users.noreply.github.com>
2022-09-15 09:12:12 -0700

    done: set_environment

--- calcio.py
+++ calcio.py
@@ -13,9 +13,10 @@ neptune-client
 import pandas as pd
 import numpy as np
 import neptune.new as neptune
+import os
+import tarfile
 import pickle
 from gensim.models import KeyedVectors
-import subprocess
 from glob import glob
 from tqdm import tqdm
 from tensorflow.keras.models import load_model
@@ -25,21 +26,27 @@ pd.options.display.max_rows = 100
 print(pd.__version__)
 print(np.__version__)
 
-
-def run_bash(bashCommand: str, nameCommand=""):
-    process = subprocess.Popen([bashCommand], shell=True)
-    _, error = process.communicate()
-    if error:
-        print(f"{nameCommand} error:\n", error)
-
-
 def get_credential(frmwork="neptune_team"):
     with open("credential.txt", "r") as container:
         for line in container:
             if frmwork in line:
                 login, psw = line.split(" ")[1], line.split(" ")[2].split("\n")[0]
                 return login, psw
-
+def unpack_tar(file_path:str):
+    if file_path[-6:] == 'tar.gz':
+        if os.path.isfile(file_path):
+            print(f'Распаковываем {file_path}')
+            with tarfile.open(file_path, 'r:gz') as targz:
+                targz.extractall()
+            print('Удалям архив')
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+            else:
+                print('Не получилось удалить архив после распаковки')
+        else:
+            print(f'Файл {file_path} не существует')
+    else:
+        print(f'Файл {file_path} не имеет расширение tar.gz')
 
 def neptune_download(saved_name: str, local_path: str):
     '''
@@ -52,11 +59,49 @@ def neptune_download(saved_name: str, local_path: str):
     project.stop()
     print('Ok...')
 
+def load_model(folder_name:str, model_type = 'HOME', model_num = 1):
+    '''
+    :param folder_name:
+    :param model_num:
+    :param model_type:
+    :return:
+    '''
+    PATH_TO_MODEL = folder_name + 'model.tar.gz'
+    _, api_key = get_credential()
+    neptune_model = f'FOOT-' + model_type
+    neptune_model_version = f'FOOT-' + model_type + '-' + str(model_num)
+    model_version = neptune.init_model_version(
+        project='scomesse/football',
+        model=neptune_model,
+        api_token=api_key,
+        version=neptune_model_version
+    )
+    print(f'Загружаем модель {model_type} n.{model_num}')
+    model_version['model'].download(PATH_TO_MODEL)
+    model_version.stop()
+    print(f'Распаковываем модель {model_type} n.{model_num}')
+    unpack_tar(PATH_TO_MODEL)
 
-def set_environment():
-    username, api_key = get_credential()
-    print('Скачиваем граф матчей team_time_dict.pickle...')
-    neptune_download('data/team_time_dict', './team_time_dict.pickle')
+def set_environment(local_folder = './'):
+
+    env_dict = {
+        'data/team_time_dict':'team_time_dict.pickle',
+        'data/team_league_dict':'team_league_dict.pickle',
+        'data/season_dict':'season_dict.pickle',
+        'data/idx_home_current_dict':'idx_home_current_dict.pickle',
+        'data/idx_away_current_dict':'idx_away_current_dict.pickle',
+        'data/idx_home_dict':'idx_home_dict.pickle',
+        'data/idx_away_dict':'idx_away_dict.pickle',
+        'data/word2vec_220811':'w2v_model.tar.gz'
+    }
+
+    for cnt, env in enumerate(env_dict.items()):
+        saved_name, file_name = env
+        print(f'Скачиваем: {file_name}...{cnt + 1}/{len(env_dict)}')
+        neptune_download(saved_name, local_folder + file_name)
+
+    unpack_tar(local_folder + env_dict['data/word2vec_220811'])
+    load_model(folder_name = './', model_num = 1, model_type='HOME')
 
 if __name__ == "__main__":
-    set_environment()
+    set_environment(local_folder = './')
