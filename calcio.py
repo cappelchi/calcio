090abc3fe5b4ce5695637cf7e19b8145afb416e2
Mikhail <41973795+cappelchi@users.noreply.github.com>
2022-09-15 00:52:03 -0700

    added: current commit

--- calcio.py
+++ calcio.py
@@ -11,6 +11,31 @@ gensim=4.2.0
 neptune-client
 '''
 
-import neptune.new as neptune
 import pandas as pd
 import numpy as np
+import pickle
+from gensim.models import KeyedVectors
+import subprocess
+from glob import glob
+from tqdm import tqdm
+from tensorflow.keras.models import load_model
+
+pd.options.display.max_columns = 50
+pd.options.display.max_rows = 100
+print(pd.__version__)
+print(np.__version__)
+
+def get_credential(frmwork = 'neptune_team'):
+    with open('credential.txt', 'r') as container:
+        for line in container:
+            if frmwork in line:
+                login, psw = line.split(' ')[1], line.split(' ')[2].split('\n')[0]
+                return login, psw
+
+username, api_key = get_credential()
+def set_environment():
+    pass
+
+
+if __name__ == '__main__':
+    print('Start here')
